894bc6b9f2151d8384a55169986a96723b181b32
NikhilRaghavan45 <135696240+NikhilRaghavan45@users.noreply.github.com>
2023-12-01 17:30:21 +0530

    Add files via upload

---
+++ ExampleOnetoOne/ExampleOnetoOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExampleOnetoOne;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Company company = new Company
+        {
+            
+            CompanyName = "Hematite Company"
+        };
+        Company company1 = new Company
+        {
+
+            CompanyName = "Godrej Company"
+        };
+
+        Employee employee = new Employee
+        {
+            EmployeeId = 1,
+            EmployeeName = "Nikhil",
+            Email = "nikhil@gmail.com",
+            Company = company 
+        };
+
+        Employee employee1 = new Employee
+        {
+            EmployeeId = 2,
+            EmployeeName = "Mukhil",
+            Email = "Mukhil@gmail.com",
+            Company = company1
+        };
+
+        Console.WriteLine("Employee: " + employee.EmployeeName);
+        Console.WriteLine("Company: " + employee.Company.CompanyName);
+        Console.WriteLine("Employee: " + employee1.EmployeeName);
+        Console.WriteLine("Company: " + employee1.Company.CompanyName);
+    }
+
+}
