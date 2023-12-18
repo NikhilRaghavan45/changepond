d9633782579b0a2b1bb5832b5f9eda15558b0738
NikhilRaghavan45 <135696240+NikhilRaghavan45@users.noreply.github.com>
2023-12-18 17:56:21 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace WebDemo.Models
+{
+    public class Employee
+    {
+
+        public string name { get; set; }
+
+        public int age { get; set; }    
+
+        public decimal salary { get;set; }
+
+        public string department { get; set; }
+
+        public char gender { get; set; }
+
+
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDemo.Models;
+using System.Linq;
+
+namespace WebDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(Repository.AllEmployees);
+        }
+
+       
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Employee employee)
+        {
+            Repository.Create(employee);
+            return View("Success",employee);
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmployees.Where(e => e.name == empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Update(string empname)
+        {
+            Employee employee = Repository.AllEmployees.Where(e => e.name == empname).FirstOrDefault();
+            return View(employee);
+        }
+
+        [HttpPost]
+
+        public IActionResult Update(Employee employee,string empname)
+        {
+            Repository.AllEmployees.Where(e => e.name == empname).FirstOrDefault().age = employee.age;
+            Repository.AllEmployees.Where(e => e.name == empname).FirstOrDefault().salary = employee.salary;
+            Repository.AllEmployees.Where(e => e.name == empname).FirstOrDefault().department = employee.department;
+            Repository.AllEmployees.Where(e => e.name == empname).FirstOrDefault().gender = employee.gender;
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
